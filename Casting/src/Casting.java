9904a00bf975a8a65d0024ac24fc2cbb8db8be64
Albert Henrique <albertmine8@gmail.com>
2025-11-18 00:06:11 -0300

    att

---
+++ Casting/src/Casting.java
@@ -0,0 +1,14 @@
+
+public class Casting {
+
+     public static void main(String args[]) {
+
+         double d = 100;
+         System.out.println(d);
+         Double d1 = d;
+         System.out.println(d1);
+     }
+
+
+
+}
