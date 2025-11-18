0a22dcaf66ad14da063e95ccdd672841510cf485
Albert Henrique <albertmine8@gmail.com>
2025-11-18 20:17:00 -0300

    att

--- Casting/src/Casting.java
+++ Casting/src/Casting.java
@@ -3,19 +3,14 @@ import java.util.Scanner;
 public class Casting {
 
      public static void main(String args[]) {
-         double d = 10;
-         Double d1 = d;
 
          Scanner sc = new Scanner(System.in);
          System.out.println("Digit um numero:");
-         double numero = sc.nextDouble();
-         System.out.println(d1);
+         double numero = (double) sc.nextDouble();
+         System.out.println(numero);
          sc.close();
 
 
-
      }
-
-
 
 }
